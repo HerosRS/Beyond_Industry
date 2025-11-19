63610a0e92634fe9389da04433e9d0bf17c6ec66
Luca Theis <lucatheis@t-online.de>
2025-11-19 22:27:02 +0100

    New

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+// Fenster erstellen
+Raylib.InitWindow(800, 600, "Beyond Industry");
+Raylib.SetTargetFPS(60);
+
+// Game Loop - läuft 60x pro Sekunde
+while (!Raylib.WindowShouldClose())  // Läuft bis X gedrückt wird
+{
+    // ===== UPDATE (Logik) =====
+    
+    // ===== DRAW (Zeichnen) =====
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.DarkGreen);  // Grüner Hintergrund
+    Raylib.DrawText("Hello Factory!", 100, 100, 40, Color.White);
+    Raylib.EndDrawing();
+}
+
+// Aufräumen
+Raylib.CloseWindow();
